d009df792b2c2940a515821298b5073cbc964b1f
reetou <fillmasterson@gmail.com>
2022-10-27 21:48:12 +0200

    Remove console logs

--- src/pages/r/[id].tsx
+++ src/pages/r/[id].tsx
@@ -78,9 +78,6 @@ const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = (
     }
   };
 
-  console.log("API URL", process.env.NEXT_PUBLIC_API_URL);
-  console.log("NEXT API URL", NEXT_API_URL);
-
   return (
     <div>
       <Head>
